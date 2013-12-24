8fc4bc5d86aa9faca5166022b704eb3ff5edf6e8
Brandon Archer <m37a11@gmail.com>
2013-12-24 13:12:09 -0700

    Bug fixes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name="pynessus-api",
+setup(name="nessus",
       version='1.1',
       description='Simple python client for Nessus 5.x',
       author='Brandon Archer',
       author_email='m37a11@gmail.com',
-      packages=['pynessus-api'],
+      packages=['nessus'],
       scripts=['scripts/nessus-client']
     )
     
